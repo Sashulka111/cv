89142b2c06921145983318f8a5d10fa7ce3623c6
Sashulka111 <alexis.grukhal@gmail.com>
2024-11-05 16:39:12 -0500

    jjjjjhhh

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ menu.addEventListener('click',function(event){
     let activeBtn = document.querySelector('.active');
 
     if(clickTarget.classList.contains ('nav-link')) {
-        clickTarget.classList.add('active')
-        activeBtn.classList.remove('active')
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active');
+
+        let mobile=document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide');
+        }
     }
 });
 window.onscroll = function(){
@@ -40,6 +45,16 @@ if (!newActiveBtn.classList.contains('active')) {
     newActiveBtn.classList.add('active');
     activeBtn.classList.remove('active')
 }
-
+document.querySelector(".mobile-button").addEventListener("click", function(event){
+    document.querySelector(".mobile-menu").classList.toggle("hide");
+    let body=document.querySelector("body");
+    let mobileMenu=document.querySelector(".mobile-menu");
+    if (!body.classList.contains("off-scroll") && !mobileMenu.classList.contains('hide')){
+       body.classList.add("off-scroll");
+    }
+    else{
+        body.classList.remove("off-school");
+    }
+});
 
 
